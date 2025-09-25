5a03a10ac7a970c67a018822c9956969c2dc992b
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-09-25 19:50:19 +0530

    Time: 0 ms (100%), Space: 53.2 MB (87.96%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.js
+++ 1952-three-divisors/1952-three-divisors.js
@@ -3,12 +3,17 @@
  * @return {boolean}
  */
 var isThree = function(n) {
-    let count = 0;
-    for (let i = 1; i <= n; i++) {
-        if (n % i === 0) {
-            count++;
-        }    
+     let count=0
+    for(let i=1;i<=n;i++){
+        
+      
+        if(n%i==0){
+            count++
+        }
     }
-    if (count === 3) return true;
-    else return false;
+    if(count==3){
+        return true
+    }else{
+        return false
+    }
 };
