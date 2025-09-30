4fb8ddf32680162a85ca22433c9ea4b40a709f16
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-09-30 10:25:18 +0530

    Time: 1 ms (96.44%), Space: 55.7 MB (59.66%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,14 +4,9 @@
  * @return {character}
  */
 var findTheDifference = function (s, t) {
-    let totalSum = 0;
-    let sum = 0;
-    for (let i = 0; i < t.length; i++) {
-        totalSum += t.charCodeAt(i);
-    }
-    for (let i = 0; i < s.length; i++) {
-        sum += s.charCodeAt(i);
-    }
+    let totalSum = t.split('').reduce((acc, curr) => acc + curr.charCodeAt(0), 0);
+    let sum = s.split('').reduce((acc, curr) => acc + curr.charCodeAt(0), 0);
+    
     let diff = totalSum - sum;
     return String.fromCharCode(diff);
 };
