d02da7bdf2ad35eb064c9e93db8eb538e7abd010
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-10-02 08:39:21 +0530

    Time: 0 ms (100%), Space: 54 MB (54.81%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.js
+++ 1816-truncate-sentence/1816-truncate-sentence.js
@@ -4,10 +4,5 @@
  * @return {string}
  */
 var truncateSentence = function (s, k) {
-    const arr = s.split(' ');
-    const newArr = []
-    for (let i = 0; i < k; i++) {
-        newArr.push(arr[i]);
-    }   
-    return newArr.join(' ');
+    return s.split(' ').slice(0, k).join(' ');
 };
