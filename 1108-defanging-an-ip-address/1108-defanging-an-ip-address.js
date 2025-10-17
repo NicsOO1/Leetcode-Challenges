23073e0e42b33cedee8b5acfb3f6929f8d156fd7
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-10-17 09:38:19 +0530

    Time: 53 ms (13.67%), Space: 54.1 MB (26.94%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.js
@@ -2,12 +2,6 @@
  * @param {string} address
  * @return {string}
  */
-var defangIPaddr = function(address) {
-    let str = '';
-   const arr = address.split('');
-   for (let item of arr) {
-    if (item != '.') str += item;
-    else str += '[.]'
-   }
-   return str;
+var defangIPaddr = function (address) {
+    return address.replaceAll('.', '[.]')
 };
