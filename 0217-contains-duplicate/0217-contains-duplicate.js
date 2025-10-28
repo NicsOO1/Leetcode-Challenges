293b40615b9625b056e52819a511c21eb9deea1e
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-10-28 19:31:25 +0530

    Time: 17 ms (73.23%), Space: 74.3 MB (38.75%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,18 +2,11 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums) {
-    const arr = [];
-    let isDuplicate = false;
-
-    for (const num of nums) {
-        for (const item of arr) {
-            if (item === num) {
-                isDuplicate = true;
-                return isDuplicate;
-            }
-        }
-        if (!isDuplicate) arr.push(num);
+var containsDuplicate = function (nums) { 
+    const set = new Set();
+    for (let num of nums) {
+        if (set.has(num)) return true;
+        set.add(num);
     }
-    return isDuplicate;
+    return false
 };
