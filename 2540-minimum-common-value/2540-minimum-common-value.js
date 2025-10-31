753346a36014ef81c0f0fb443e06098edc1e544a
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-10-31 10:41:58 +0530

    Time: 16 ms (25.5%), Space: 76.9 MB (6.71%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -8,11 +8,13 @@ var getCommon = function (nums1, nums2) {
     const sortedArr = nums2.sort((a, b) => a - b)
     const set = new Set([...nums1])
     for (let item of sortedArr) {
-    if (set.has(item))  commonValue = item
-        break;
+        if (set.has(item)) {
+            commonValue = item;
+            break;
+        }
     }
 
-    if(commonValue !== 0) return commonValue;
+    if (commonValue !== 0) return commonValue;
     else return -1;
 
 };
