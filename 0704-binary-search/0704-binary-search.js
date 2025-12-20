06fed3dbd70260822c95e032d84e8ef75f50931c
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2025-12-20 19:21:14 +0530

    Time: 0 ms (100%), Space: 57.6 MB (68.52%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,8 +4,20 @@
  * @return {number}
  */
 var search = function (nums, target) {
-   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) return i;
-   }
-   return -1
+    let left = 0;
+    let right = nums.length - 1;
+    const mid = Math.floor((left + right) / 2);
+
+    if (target === nums[mid]) return mid;
+    else if (target < nums[mid]) {
+        for (let i = left; i < mid; i++) {
+            if (nums[i] === target) return i;
+        }
+    }
+    else if (target > nums[mid]) {
+        for (let i = right; i > mid; i--) {
+            if (nums[i] === target) return i;
+        }
+    }
+   return -1;
 };
